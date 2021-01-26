a637585647fa3bd617f9081174eb076caeadd9ef
JKamlah <jan.kamlah@gmail.com>
2021-01-26 14:36:29 +0100

    Update pdf controller.

--- tesseractXplore/pdf.py
+++ tesseractXplore/pdf.py
@@ -3,21 +3,23 @@ from functools import partial
 from logging import getLogger
 from os import makedirs
 from pathlib import Path
-from subprocess import Popen
-from subprocess import getstatusoutput, check_output
+import re
+from subprocess import Popen, run, getstatusoutput, check_output
 from sys import platform as _platform
 
 from kivymd.toast import toast
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
-from kivymd.uix.list import MDList
-from kivymd.uix.list import OneLineListItem, OneLineAvatarListItem
+from kivymd.uix.list import MDList, OneLineListItem, OneLineAvatarListItem
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.boxlayout import MDBoxLayout
 
-from tesseractXplore.app import alert
-from tesseractXplore.app import get_app
+from tesseractXplore.widgets import MyToggleButton
+from tesseractXplore.app import alert, get_app
 from tesseractXplore.constants import PDF_DIR
 from tesseractXplore.widgets.lists import SwitchListItem
+from tesseractXplore.widgets.progressbar import MDProgressBar
+
 
 logger = getLogger().getChild(__name__)
 
@@ -28,22 +30,18 @@ def pdf_dialog(pdfpath):
         instance.parent.parent.parent.parent.dismiss()
 
     layout = MDList()
-    if getstatusoutput("pdfimages")[0] not in [1, 127]:
-        import re
-        pdfinfos = check_output(["pdfimages", "-list", pdfpath]).decode('utf-8')
-        pdfinfos = re.sub(r' +', ' ', pdfinfos)
-        pdfinfos = pdfinfos.split("\n")[2:-1]
-        pages = str(len(pdfinfos))
-        if pages != "0":
-            dpis = [pdfinfo.split(" ")[-3] for pdfinfo in pdfinfos]
-            from collections import Counter
-            dpi = Counter(dpis).most_common(1)[0][0]
-        else:
-            pdfinfos = check_output(["pdfinfo", pdfpath]).decode('utf-8')
-            pages = pdfinfos.split("\n")[9].split(": ")[-1].strip()
-            dpi = 300
+    pdfinfos = check_output(["pdfimages", "-list", pdfpath]).decode('utf-8')
+    pdfinfos = re.sub(r' +', ' ', pdfinfos)
+    pdfinfos = pdfinfos.split("\n")[2:-1]
+    pages = str(len(pdfinfos))
+    if pages != "0":
+        dpis = [pdfinfo.split(" ")[-3] for pdfinfo in pdfinfos]
+        from collections import Counter
+        dpi = Counter(dpis).most_common(1)[0][0]
     else:
-        pages = ""
+        pdfinfos = check_output(["pdfinfo", pdfpath]).decode('utf-8')
+        pages = pdfinfos.split("\n")[9].split(": ")[-1].strip()
+        dpi = 300
     layout.add_widget(OneLineListItem(text=f'The detected resolution is: {dpi}'))
     layout.add_widget(OneLineListItem(text='First page'))
     # id first
@@ -52,9 +50,6 @@ def pdf_dialog(pdfpath):
     # id last
     layout.add_widget(MDTextField(text=pages, hint_text="Last page", height=400))
     layout.add_widget(OneLineListItem(text='Imageformat (jpg, jp2, png, ppm(default), tiff)'))
-
-    from kivymd.uix.boxlayout import MDBoxLayout
-    from tesseractXplore.widgets import MyToggleButton
     # id = "fileformat"
     boxlayout = MDBoxLayout(orientation="horizontal", adaptive_height=True)
     boxlayout.add_widget(MyToggleButton(text="jpeg", group="imageformat"))
@@ -71,7 +66,6 @@ def pdf_dialog(pdfpath):
     defaulttoggle = MyToggleButton(text="extraction", group="converting")
     boxlayout.add_widget(defaulttoggle)
     layout.add_widget(boxlayout)
-    # layout.add_widget(MDTextField(id="fileformat", text="jpg", hint_text="Fileformat", height=400))
     # id='include_pagenumber',
     pagenumbers = OneLineAvatarListItem(text='Include page numbers in output file names')
     # id = 'include_pagenumber_chk'
@@ -113,16 +107,14 @@ def open_pdf(fname, *args):
         import webbrowser
         webbrowser.open(str(Path(fname).absolute()))
     else:
-        import subprocess
         try:
-            subprocess.run([pdfviewer, str(Path(fname).absolute())])
+            run([pdfviewer, str(Path(fname).absolute())])
         except:
             alert(f"Couldn't find: {pdfviewer}")
             pass
 
 
 def pdfimages(pdfpath, instance, *args):
-    from tesseractXplore.widgets.progressbar import MDProgressBar
     pb = MDProgressBar(color=get_app().theme_cls.primary_color, type="indeterminate")
     status_bar = get_app().image_selection_controller.status_bar
     status_bar.clear_widgets()
@@ -137,7 +129,7 @@ def pdfimages(pdfpath, instance, *args):
         if idx == 6:
             for fileformat in child.children:
                 if fileformat.state == 'down':
-                    fileformat.text = "j" if fileformat.text == "jpg" and process == "pdfimages" else fileformat.text
+                    fileformat.text = "j" if fileformat.text == "jpeg" and process == "pdfimages" else fileformat.text
                     fileformat.text = "jpeg" if fileformat.text == "jp2" and process == "pdfimages" else fileformat.text
                     params.extend([f"-{fileformat.text}"])
         if idx == 2 and child.text != "":
@@ -173,15 +165,14 @@ def extract_pdf(pdfpath):
             # TODO: Don work atm properly and use the official site
             url = 'https://digi.bib.uni-mannheim.de/~jkamlah/poppler-20.12.1-h7cbfaf2_0.tar.bz2'
             try:
-                pass
-                # import requests, zipfile, io
-                # r = requests.get(url, stream=True)
-                # pdftoolpath.mkdir(parents=True)
-                # z = zipfile.ZipFile(io.BytesIO(r.content))
-                # z.extractall(str(pdftoolpath.absolute()))
-                # logger.info(f'Download: Succesful')
+                import requests, zipfile, io
+                r = requests.get(url, stream=True)
+                pdftoolpath.mkdir(parents=True)
+                z = zipfile.ZipFile(io.BytesIO(r.content))
+                z.extractall(str(pdftoolpath.absolute()))
+                logger.info(f'Download: Succesful')
                 # Update Modelslist
-                # get_app().tesseract_controller.models = get_app().tesseract_controller.get_models()
+                get_app().tesseract_controller.models = get_app().tesseract_controller.get_models()
             except:
                 logger.info(f'Download: Error while downloading')
     return pdfpath
