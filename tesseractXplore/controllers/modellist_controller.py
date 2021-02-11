66ed38b2809adb036da5b447b1c53e67f6274779
JKamlah <jan.kamlah@gmail.com>
2021-02-11 12:57:17 +0100

    ADD multimodel selection for ocr.

--- tesseractXplore/controllers/modellist_controller.py
+++ tesseractXplore/controllers/modellist_controller.py
@@ -1,7 +1,7 @@
 from functools import partial
 
 from kivy.properties import StringProperty
-from kivymd.uix.list import OneLineListItem
+from kivymd.uix.list import OneLineListItem, OneLineAvatarIconListItem, TwoLineAvatarIconListItem, MDList
 
 from tesseractXplore.app import get_app
 from tesseractXplore.controllers import Controller
@@ -10,26 +10,41 @@ from tesseractXplore.controllers import Controller
 class CustomOneLineListItem(OneLineListItem):
     icon = StringProperty()
 
+from kivymd.uix.list import ILeftBodyTouch
+from kivymd.uix.selectioncontrol import MDCheckbox
+class LeftCheckbox(ILeftBodyTouch, MDCheckbox):
+    '''Custom right container.'''
+
 
 class ModelListController(Controller):
     """ Controller class to manage image metadata screen """
 
     def __init__(self, screen, **kwargs):
         self.screen = screen
+        self.layout = MDList()
+        self.checked_models = None
+        self.screen['model_selection_button'].bind(on_release=self.set_model_btn)
 
     def set_list(self, text="", search=False):
         '''Lists all installed models '''
 
         def add_item(model):
-            self.screen.modellist.data.append(
-                {
-                    "viewclass": "OneLineListItem",
-                    "text": model,
-                    "on_release": partial(self.set_model, model)
-                }
+            item = OneLineAvatarIconListItem(
+                text=model,
+                secondary_text="",
+                on_release=partial(self.set_model, model),
             )
+            item.add_widget(LeftCheckbox(active=self.checked_models[model]))
+            self.layout.add_widget(item)
 
-        self.screen.modellist.data = []
+        if self.checked_models is None:
+            self.checked_models = {}
+            for model in get_app().tesseract_controller.models:
+                self.checked_models[model] = False
+        else:
+            self.chk_active_models()
+        self.layout.clear_widgets()
+        self.screen.modellist.clear_widgets()
         for model in get_app().tesseract_controller.models:
             if search:
                 if self.screen.exact_match_chk.active:
@@ -42,7 +57,21 @@ class ModelListController(Controller):
                         add_item(model)
             else:
                 add_item(model)
+        self.screen.modellist.add_widget(self.layout)
 
-    def set_model(self, model):
-        get_app().tesseract_controller.screen.model.set_item('Model: ' + model)
+    def chk_active_models(self):
+        for model in self.layout.children:
+            self.checked_models[model.children[2].children[2].text] = model.children[1].children[0].active
+
+    def set_model_btn(self, instance, *args):
+        self.set_model("")
+
+    def set_model(self, model, *args):
+        selected_models = []
+        if model != "": selected_models.append(model)
+        self.chk_active_models()
+        for chk_model, state in self.checked_models.items():
+            if state and chk_model != model:
+                selected_models.append(chk_model)
+        get_app().tesseract_controller.screen.model.set_item('Model: ' + '+'.join(selected_models))
         get_app().switch_screen('tesseract_xplore')
