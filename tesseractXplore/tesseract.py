9de1a2ca78eb1b8d66d7dae8b9bb970a868a362a
JKamlah <jan.kamlah@gmail.com>
2021-02-22 11:57:23 +0100

    Rework tesseract exe download.

--- tesseractXplore/tesseract.py
+++ tesseractXplore/tesseract.py
@@ -6,6 +6,7 @@ from subprocess import Popen, PIPE,DEVNULL, STDOUT
 from pathlib import Path
 
 import requests
+from kivy.network.urlrequest import UrlRequest
 from kivymd.uix.list import OneLineListItem
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -58,26 +59,35 @@ def install_tesseract(instance):
     else:
         run_cmd_with_sudo_dialog(title="Enter sudo password to change the rights of the destination folder",func=install_tesseract_unix)
 
-def install_tesseract_win():
+def dl_install_tesseract_win():
     try:
         if _platform == "win32":
             url = get_app().settings_controller.tesseract['win32url']
         else:
             url = get_app().settings_controller.tesseract['win64url']
-        r = requests.get(url)
-        fout = Path(tempfile.gettempdir()).joinpath("tesseract.exe")
-        logger.info(f"Creating: {fout}")
-        with open(fout, 'wb') as f:
-            f.write(r.content)
-        toast('Download: Succesful')
-        logger.info(f'Download: Succesful')
-        startfile(fout)
-        reset_tesspaths()
-        get_app().stop()
+        from kivymd.uix.progressbar import MDProgressBar
+        pb = MDProgressBar(color=get_app().theme_cls.primary_color, type="indeterminate")
+        status_bar = get_app().image_selection_controller.status_bar
+        status_bar.clear_widgets()
+        status_bar.add_widget(pb)
+        pb.start()
+        def update_progress(request, current_size, total_size):
+            pb.value = current_size / total_size
+        UrlRequest(url = url, on_progress = update_progress,
+        chunk_size = 1024, on_success = install_tesseract_win,
+        file_path = Path(tempfile.gettempdir()).joinpath("tesseract.exe"))
+
     except Exception as e:
         print(e)
         toast('Download: Error')
         logger.info(f'Download: Error while downloading')
+
+def install_tesseract_win():
+    toast('Download: Succesful')
+    logger.info(f'Download: Succesful')
+    startfile(Path(tempfile.gettempdir()).joinpath("tesseract.exe"))
+    reset_tesspaths()
+    get_app().stop()
 
 def install_tesseract_unix(instance, *args):
     pwd = instance.parent.parent.parent.parent.content_cls.children[0].text
